976b649845f2db3640bfc9edb7d2b3e37c03d0f9
Marta Molina <marta.molina@live.u-tad.com>
2022-02-03 00:25:11 +0100

    Starting using insert of List of taskList

--- app/src/main/java/com/marta/ud6_01_networkud6/model/TaskList.kt
+++ app/src/main/java/com/marta/ud6_01_networkud6/model/TaskList.kt
@@ -1,7 +1,11 @@
 package com.marta.ud6_01_networkud6.model
 
+import com.marta.ud6_01_networkud6.provider.db.entitties.TaskListEntity
+
 data class TaskList(
     val listId: Int,
     val name: String,
     val userFk: Int
 )
+fun TaskList.toEntityTaskList(): TaskListEntity = TaskListEntity(listId,name, userFk)
+fun List<TaskList>.toListOfEntityList(): List<TaskListEntity> =map { it.toEntityTaskList() }
--- app/src/main/java/com/marta/ud6_01_networkud6/usescases/tasks/TaskListFragment.kt
+++ app/src/main/java/com/marta/ud6_01_networkud6/usescases/tasks/TaskListFragment.kt
@@ -7,12 +7,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.marta.ud6_01_networkud6.databinding.FragmentTaskListBinding
 import com.marta.ud6_01_networkud6.model.TaskList
+import com.marta.ud6_01_networkud6.model.toListOfEntityList
 import com.marta.ud6_01_networkud6.provider.api.TaskApi
+import com.marta.ud6_01_networkud6.provider.db.DataBaseRepository
+import com.marta.ud6_01_networkud6.provider.db.entitties.TaskListEntity
 import com.marta.ud6_01_networkud6.usescases.common.TaskListAdapter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -23,7 +29,7 @@ class TaskListFragment : Fragment() {
     private val binding
         get() = _binding!!
     private val lista: MutableList<TaskList> = mutableListOf()
-    private val adapter = TaskListAdapter({deleteList(it.listId)}){
+    private val adapter = TaskListAdapter({ deleteList(it.listId) }) {
         viewChange(it.listId, it.name)
     }
 
@@ -45,85 +51,112 @@ class TaskListFragment : Fragment() {
             val text = binding.tfNewList.text.toString()
             addList(text)
         }
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
     // Elements visibility
-    private fun showHideMessage(){
-        if(lista.size>0){
+    private fun showHideMessage() {
+        if (lista.size > 0) {
             binding.tvNoList.visibility = View.INVISIBLE
-        }else{
+        } else {
             binding.tvNoList.visibility = View.VISIBLE
         }
     }
+    private fun clearText(){
+        binding.tfNewList.setText("")
+    }
+
     //Req data
-    private fun requestTaskList(){
+    private fun requestTaskList() {
         val service = TaskApi.service.getTaskLists()
-        val call = service.enqueue(object : Callback<List<TaskList>>{
+        val call = service.enqueue(object : Callback<List<TaskList>> {
             override fun onResponse(
                 call: Call<List<TaskList>>,
                 response: Response<List<TaskList>>
             ) {
-                if(response.isSuccessful){
-                    if (lista.size>0){
+                if (response.isSuccessful) {
+                    if (lista.size > 0) {
                         lista.clear()
                     }
                     response.body()?.let { lista.addAll(it) }
                     adapter.submitList(lista)
                     adapter.notifyDataSetChanged()
+                    lifecycleScope.launch(Dispatchers.IO){
+                        DataBaseRepository.getInstance(requireContext()).databaseDao().addAllLists(lista.toListOfEntityList())
+                    }
                     showHideMessage()
-                }else{
-                    Toast.makeText(context, "RESPONSE(╯°□°）╯︵ ┻━┻ Connection faliure", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(
+                        context,
+                        "RESPONSE(╯°□°）╯︵ ┻━┻ Connection faliure",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
+
             override fun onFailure(call: Call<List<TaskList>>, t: Throwable) {
-                Toast.makeText(context, "FALIURE(╯°□°）╯︵ ┻━┻ Connection faliure ", Toast.LENGTH_SHORT).show()
-                Log.e("faliure","$t")
+                Toast.makeText(
+                    context,
+                    "FALIURE(╯°□°）╯︵ ┻━┻ Connection faliure ",
+                    Toast.LENGTH_SHORT
+                ).show()
+                Log.e("faliure", "$t")
             }
         })
     }
+
     //Add
-    private fun addList(title: String){
-        val newList: TaskList = TaskList((lista.size+1), title, 1)
+    private fun addList(title: String) {
+        val newList: TaskList = TaskList((lista.size + 1), title, 1)
         val service = TaskApi.service.addList(newList)
-        val call = service.enqueue(object : Callback<Any>{
+        val call = service.enqueue(object : Callback<Any> {
             override fun onResponse(call: Call<Any>, response: Response<Any>) {
-                if(response.isSuccessful){
+                if (response.isSuccessful) {
                     lista.add(newList)
                     adapter.submitList(lista)
                     adapter.notifyDataSetChanged()
                     showHideMessage()
+                    clearText()
                     Toast.makeText(context, "Saved", Toast.LENGTH_SHORT).show()
-                }else{
-                    Toast.makeText(context, "(╯°□°）╯︵ ┻━┻ Format faliure", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(context, "(╯°□°）╯︵ ┻━┻ Format faliure", Toast.LENGTH_SHORT)
+                        .show()
                 }
             }
+
             override fun onFailure(call: Call<Any>, t: Throwable) {
-                Toast.makeText(context, "(╯°□°）╯︵ ┻━┻ Connection faliure ", Toast.LENGTH_SHORT).show()
-                Log.e("faliure","$t")
+                Toast.makeText(context, "(╯°□°）╯︵ ┻━┻ Connection faliure ", Toast.LENGTH_SHORT)
+                    .show()
+                Log.e("faliure", "$t")
             }
         })
     }
-    private fun deleteList(listId: Int){
+
+    private fun deleteList(listId: Int) {
         val service = TaskApi.service.deleteList(listId)
         val call = service.enqueue(object : Callback<Int> {
             override fun onResponse(call: Call<Int>, response: Response<Int>) {
-                if(!response.isSuccessful){
-                    Log.d("Item","(╯°□°）╯︵ ┻━┻ Formato incorrecto")
-                }else{
+                if (!response.isSuccessful) {
+                    Log.d("Item", "(╯°□°）╯︵ ┻━┻ Formato incorrecto")
+                } else {
                     requestTaskList()
                 }
             }
+
             override fun onFailure(call: Call<Int>, t: Throwable) {
-                Log.d("Item","(╯°□°）╯︵ ┻━┻ Formato incorrecto $t")
+                Log.d("Item", "(╯°□°）╯︵ ┻━┻ Formato incorrecto $t")
             }
         })
     }
-    private fun viewChange(listId: Int, listName:String){
-        val action = TaskListFragmentDirections.actionTaskListFragmentToTasksFragment(listId,listName)
+
+    private fun viewChange(listId: Int, listName: String) {
+        val action =
+            TaskListFragmentDirections.actionTaskListFragmentToTasksFragment(listId, listName)
         findNavController().navigate(action)
     }
 }
