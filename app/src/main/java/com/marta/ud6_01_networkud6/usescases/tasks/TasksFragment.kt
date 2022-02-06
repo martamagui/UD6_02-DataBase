f05e9fe2ab1fcd31b4ab1fb40abe2ea9244a8f9b
Marta Molina <marta.molina@live.u-tad.com>
2022-02-06 13:22:27 +0100

    Not crashing any more :smile:

--- app/src/main/java/com/marta/ud6_01_networkud6/usescases/tasks/TasksFragment.kt
+++ app/src/main/java/com/marta/ud6_01_networkud6/usescases/tasks/TasksFragment.kt
@@ -1,7 +1,6 @@
 package com.marta.ud6_01_networkud6.usescases.tasks;
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -16,7 +15,6 @@ import com.marta.ud6_01_networkud6.provider.db.entitties.ListWithTasks
 import com.marta.ud6_01_networkud6.provider.db.entitties.TaskEntity
 import com.marta.ud6_01_networkud6.usescases.common.TaskAdapter
 import kotlinx.coroutines.*
-import okhttp3.internal.wait
 
 
 class TasksFragment : Fragment() {
@@ -24,8 +22,6 @@ class TasksFragment : Fragment() {
     private val binding
         get() = _binding!!
     private val args: TasksFragmentArgs by navArgs()
-    private var listAndTasks: ListWithTasks? = null
-    private var tasks: MutableList<TaskEntity> = mutableListOf()
     private var listId: Int = 0
     private val adapter = TaskAdapter {
         toDetailView(it.taskId!!)
@@ -36,14 +32,16 @@ class TasksFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentTasksBinding.inflate(inflater, container, false)
-        getListWithTasksFromDB()
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setUI()
-        getTasks()
+        viewLifecycleOwner.lifecycleScope.launch(Dispatchers.Main) {
+            val listWithTasks = getListWithTasksFromDB()
+            getTasks(listWithTasks)
+        }
     }
 
     override fun onDestroyView() {
@@ -69,19 +67,18 @@ class TasksFragment : Fragment() {
         binding.rvTasks.layoutManager = LinearLayoutManager(context)
     }
 
-    private fun showHideMessage() {
-        Log.d("list size", tasks.size.toString())
-        if (tasks.size > 0) {
+    private fun showHideMessage(listWithTasks: List<TaskEntity>?) =
+        if (!listWithTasks.isNullOrEmpty()) {
             binding.tvNoTask.visibility = View.INVISIBLE
         } else {
             binding.tvNoTask.visibility = View.VISIBLE
         }
-    }
 
-    private fun updateRV() {
-        adapter.submitList(tasks)
-        showHideMessage()
-        adapter.notifyDataSetChanged()
+    private fun updateRV(listWithTasks: ListWithTasks) {
+        if (listWithTasks.tasks?.size ?: -1 > 0) {
+            adapter.submitList(listWithTasks.tasks!!)
+        }
+        showHideMessage(listWithTasks.tasks)
     }
 
     private fun deletedListview() {
@@ -104,12 +101,10 @@ class TasksFragment : Fragment() {
 
 
     //DB
-    fun getListWithTasksFromDB() {
-        lifecycleScope.launch(Dispatchers.IO) {
-            tasks.clear()
-            listAndTasks = DataBaseRepository.getInstance(requireContext()).databaseDao()
-                .findTaskFromList(listId)
-        }
+    private suspend fun getListWithTasksFromDB(): ListWithTasks {
+        val listAndTasks = DataBaseRepository.getInstance(requireContext()).databaseDao()
+            .findTaskFromList(listId)
+        return listAndTasks
     }
 
     private fun deleteList(id: Int) {
@@ -123,12 +118,9 @@ class TasksFragment : Fragment() {
 
     //Utils
 
-    private fun getTasks() {
-        if (listAndTasks?.tasks?.count()!! > 0) {
-            tasks.addAll(
-                listAndTasks?.tasks!!
-            )
-            updateRV()
+    private fun getTasks(listWithTasks: ListWithTasks) {
+        if (listWithTasks?.tasks?.count()!! > 0) {
+            updateRV(listWithTasks)
         }
     }
 }
